ef13d6a92d758d75d5ed5be3258383a977131693
changwn <changwn.nku@gmail.com>
2019-08-23 05:09:02 -0400

    add search in promotor region

--- process_atac_chrome.R
+++ process_atac_chrome.R
@@ -12,19 +12,24 @@ file_norm <- list.files()
 
 extract_atac <- list()
 cancer_str <- c()
+extarct_2more <- list()
 for(i in 1:length(file_norm))
 {
+
 	atac_norm_tmp <- read.table(file_norm[i], header=T)
 	cancer_str <- c(cancer_str, unlist(strsplit(file_norm[i], '_', fixed=T))[[1]])
+	print(cancer_str[i])
 	
 
 	found1_count <- 0
 	found2_count <- 0
 	storage_info <- data.frame()
+	more_than2_storage <- c()
 	k <- 1
 	#for(ii in 1:1000)
 	for(ii in 1:nrow(atac_norm_tmp))
 	{
+		if(ii %% 3000 == 0) print(ii)
 		#print(ii)
 		chr_tmp <- as.character(atac_norm_tmp[ii, 1])
 		start_tmp <- atac_norm_tmp[ii, 2]
@@ -45,17 +50,19 @@ for(i in 1:length(file_norm))
 			}
 			if(found_count == 2){
 				found2_count <- found2_count + 1
-				print(paste('ii : ', ii, sep=''))
-				print(paste('k = ', k, sep=''))
+				#print(paste('ii : ', ii, sep=''))
+				#print(paste('k = ', k, sep=''))
 				storage_info[k, 1] <- ii
 				storage_info[k, 2] <- atac_norm_tmp[ii, 4]
 				storage_info[k, 3] <- combine_with_semicolon(found_stat[[2]])
 				storage_info[k, 4] <- combine_with_semicolon(found_stat[[3]])
 				storage_info[k, 5] <- 2
 			}
-			if(found_count > 2)
-				print('There is the situation that found more than 2 !!!')
-
+			if(found_count > 2){
+				#print('There is the situation that found more than 2 !!!')
+				more_than2_tmp <- matrix(c(ii, found_count), 1,2)
+				more_than2_storage <- rbind(more_than2_storage, more_than2_tmp)
+			}
 
 			k <- k + 1
 		}
@@ -69,9 +76,16 @@ for(i in 1:length(file_norm))
 	colnames(atac_norm_extract)[2] <- 'gene_name'
 
 	extract_atac[[i]] <- atac_norm_extract
+	extarct_2more[[i]] <- more_than2_storage
 
 }
 names(extract_atac) <- cancer_str
+names(extarct_2more) <- cancer_str
+
+setwd('C:/Users/wnchang/Documents/F/PhD_Research/2019_08_08_atac')
+save(extract_atac, file='extract_atac_log2norm.RData')
+save(extarct_2more, file='extract_2more_info.RData')
+
 
 
 
@@ -98,6 +112,44 @@ judge_in_range <- function(chr_tmp, start_tmp, end_tmp, Homo_prot_code)
 	return(list(found_flag, found_loca, found_gene))
 }
 
+judge_in_promotor <- function(chr_tmp, start_tmp, end_tmp, Homo_prot_code)
+{
+  chr_tmp <- unlist(strsplit(chr_tmp, 'r', fixed=T))[[2]]
+  Homo_prot_code_tmp <- Homo_prot_code[which(Homo_prot_code[, 9] == chr_tmp), ]
+  
+  found_flag <- 0
+  found_loca <- c()
+  found_gene <- c()
+  found_direction <- c()
+  for(i in 1:nrow(Homo_prot_code_tmp))
+  {
+    if(Homo_prot_code_tmp[i, 8] == '+')
+    {
+      if( (Homo_prot_code_tmp[i, 6]-600) <= start_tmp && end_tmp <= Homo_prot_code_tmp[i, 6])
+      {
+        found_flag <- found_flag + 1
+        found_loca <- c(found_loca, i)
+        found_gene <- c(found_gene, Homo_prot_code_tmp[i, 3])
+        found_direction <- c(found_direction, '+')
+      }
+    }else{
+      if(Homo_prot_code_tmp[i, 7] <= start_tmp && end_tmp <= (Homo_prot_code_tmp[i, 7]+600) )
+      #if(Homo_prot_code_tmp[i, 7] = end_tmp && end_tmp <= (Homo_prot_code_tmp[i, 7]+600) )
+      #if(Homo_prot_code_tmp[i, 7] = start_tmp && start_tmp <= (Homo_prot_code_tmp[i, 7]+600) )
+      {
+        found_flag <- found_flag + 1
+        found_loca <- c(found_loca, i)
+        found_gene <- c(found_gene, Homo_prot_code_tmp[i, 3])
+        found_direction <- c(found_direction, '-')
+      }
+    }
+    
+  }
+  
+  return(list(found_flag, found_loca, found_gene, found_direction))
+}
+
+#combine the 2 element into 1 element
 combine_with_semicolon <- function(my.obj)
 {
 	combine_result <- paste0(my.obj[1], sep=';', my.obj[2])
